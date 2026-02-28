330c81a5704c231b36f0cb46e466ed5ca1efe9f5
athelll <felix.okoronkwo.1@gmail.com>
2026-02-28 19:06:49 +0100

    update to unitester

--- src/Tests/TestBenEncoder.cpp
+++ src/Tests/TestBenEncoder.cpp
@@ -9,22 +9,40 @@
 // when laptop charged overload add test; one for variadic arguemnst to be forwarded
 // the other for initializer_list to be passed to test function, since they behave
 // abnormally when used in the context of variadic templates
-struct Tester {
+struct UnitTest {
   std::deque<std::function<void()>> test_list;
 
   template <typename F, typename ER, typename ...A>
-  Tester& add_test(std::string test_tag, F func, ER expected_result,A&&... args) {
+  void add_test(std::string test_tag, F func, ER expected_result,A&&... args) {
     auto test = [=](){;
       using return_type = std::decay_t< typename std::invoke_result_t<F,A...> >;
       return_type return_value = func(std::forward<A>(args)...);
-      bool is_return_type_ptr = std::is_pointer_v<return_type>;
-      //std::remove_pointer_t<return_type> return_value_nptr = is_return_type_ptr ? *return_value : return_value;
+      std::remove_pointer_t<return_type> return_value_nptr;
+      if constexpr (std::is_pointer_v<return_type>)
+        return_value_nptr = *return_value;
+      else
+        return_value_nptr = return_value;
       bool result_equal = return_value == expected_result;
       std::cout << return_value << '\n';
-      std::cout << "Descriptor: " << test_tag << (result_equal?" succeeded":" failed") << '\n';
+      std::cout  << "Descriptor " << test_tag << (result_equal?" succeeded" :" failed") << '\n';
     };
     test_list.push_back(test);
-    return *this;
+  }
+  template <typename F, typename ER, typename A>
+  void add_test(std::string test_tag, F func, ER expected_result, A arg) {
+    auto test = [=](){
+      using return_type = std::decay_t< typename std::invoke_result_t<F, A>>;
+      return_type return_value = func(arg);
+      std::remove_pointer_t<return_type> return_value_nptr;
+      if constexpr (std::is_pointer_v<return_type>)
+        return_value_nptr = *return_value;
+      else
+        return_value_nptr = return_value;
+      bool result_equal = return_value == expected_result;
+      std::cout << return_value << '\n';
+      std::cout  << "Descriptor " << test_tag << (result_equal?" succeeded" :" failed") << '\n';
+    };
+    test_list.push_back(test);
   }
   void run_tests() {
     int test_size = test_list.size();
@@ -38,8 +56,7 @@ struct Tester {
 };
 
 int main() {
-  Tester ben_encoder_tester{};
-  ben_encoder_tester.add_test("Ben-encoder for int", Bendata::encode_to_list, "li755ee", std::initializer_list<std::string>{"i755e"})
-
+  UnitTest ben_encoder_tester{};
+  ben_encoder_tester.add_test("Ben-encoder for list", Bendata::encode_to_list, "li755ee", std::initializer_list<std::string>{"i755e"});
   ben_encoder_tester.run_tests();
 }
--- src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <iostream>
-
-int main(){
-  std::cout << std::string{'i'} + std::to_string(20) + 'e';
-
-}
