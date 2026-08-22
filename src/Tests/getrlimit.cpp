f76b5bc01099aa00c72b260773e40645086014b9
athelll <felix.okoronkwo.1@gmail.com>
2026-08-22 16:21:44 +0100

    ch-ch-ch-changes Turn and face the strange

--- src/Tests/getrlimit.cpp
+++ src/Tests/getrlimit.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
   struct rlimit* result;
-  result = malloc(sizeof(struct rlimit));
+  result = (rlimit*) malloc(sizeof(struct rlimit));
   memset(result, 0, sizeof(struct rlimit));
   int code = getrlimit(RLIMIT_SIGPENDING, result);
   printf("rlimt_cur: %lu \trlimt_max: %lu \n", (unsigned long)result->rlim_cur, (unsigned long)result->rlim_max);
--- src/Tests/test.c
+++
@@ -1,9 +0,0 @@
-
-#include <pthread.h>
-#include <stdio.h>
-
-int main() {
-  int threads = pthread_getconcurrency();
-  printf("Number of threads %d\n", threads);
-  return 0;
-}
